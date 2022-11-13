f81adf3b9edb98c995a349b0697e4835923d640e
efraser25@gmail.com <efraser25@gmail.com>
2022-11-13 01:03:53 +0000

    added cells

--- test_python.py
+++ test_python.py
@@ -1,2 +1,18 @@
 # Databricks notebook source
 import sys
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # this is some md
+# MAGIC 
+# MAGIC more md:
+# MAGIC - a
+# MAGIC - b
+# MAGIC - c
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- SOME SQL
+# MAGIC select 0
